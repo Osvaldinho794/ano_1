7acdf3267fe90bd27da45168efe0830c1aa9dcd0
Osvaldinho794 <167587763+Osvaldinho794@users.noreply.github.com>
2024-04-19 22:03:58 -0300

    EX10_L3.c

--- EX10_L3.c
+++ EX10_L3.c
@@ -4,26 +4,23 @@ int main() {
 
 int maior_numero_atual = 0, numero, menor_numero_atual = 0;
 
-menor_numero_atual=numero;
-maior_numero_atual=numero;
 
 printf("Digite quantos valores você quiser e descubra o maior e o menor: \n");
-//faz com que o valor deixe de ser 0
-scanf(" %d", &numero);
-
-//para encerrar o programa devo digitar 0
-while(numero!=0) {
-
-
-		if(numero>maior_numero_atual) 
-	{
-			maior_numero_atual=numero;
-
-	}	if(numero<menor_numero_atual) {
+scanf(" %d", &numero);//esse scanf me diz o maior número
+menor_numero_atual=numero;
+maior_numero_atual=numero;
+do{
+    scanf(" %d", &numero);//esse scanf me diz o menor número
+    if(numero>0){//eu também posso fazer: if(numero<menor_numero_atual && numero>0, para que 0 não assuma o menor valor
+		
+	if(numero>maior_numero_atual){
+	    maior_numero_atual=numero;
+	    
+	}if(numero<menor_numero_atual) {
 			menor_numero_atual=numero;
 }
-scanf(" %d", &numero);
 }
+}while(numero>0);
 printf("O maior numero eh: %d\n", maior_numero_atual);
 printf("O menor numero eh: %d\n", menor_numero_atual);
 return 0;
